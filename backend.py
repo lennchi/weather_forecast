bf9cb36159e971c7febf35baed34da1ccdb53152
lennchi <elena.haramulova@gmail.com>
2024-07-28 22:22:48 +0200

    Comment out dotenv and os in backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 # load_dotenv()
 # api_key = os.getenv('API_KEY')
